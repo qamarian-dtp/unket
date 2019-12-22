00c666f247f14c1f5124264bd3b88e16a4d43ee1
Ibrahim Oladipupo Qamardeen <direct@kensharval.com>
2019-12-22 23:10:39 +0100

    Progress 23:10; Dec 22, 2019

--- doc.go
+++ doc.go
@@ -3,11 +3,11 @@
 // But what is an unket?
 //
 // Well, an unket is an abstract basket. It contains unique whole numbers
-// (1 <= x <= infinity), and numbers can be picked out of the basket. Number picking are
+// (1 <= x <= infinity), and numbers can be picked out of the basket. Number picking is
 // done randomly.
 //
 // But of what use if this?
 //
 // Unkets are used whenever deemed appropriate, but one common example is: when you want to
-// randomly pick an element of an array.
+// randomly pick an element from an array.
 package unket
